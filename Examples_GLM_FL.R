883244ba2a71c5c9ee4260d0e2ff90d14404518d
Matteo Cellamare <m.cellamare@iknl.nl>
2020-03-23 11:19:03 +0100

    make.GLM object added and some improvment

--- Examples_GLM_FL.R
+++ Examples_GLM_FL.R
@@ -1,5 +1,5 @@
 remove(list=ls())
-source("Poisson_FL.R")
+source("C:/Users/mce1908.52713/Desktop/FEDERATED_GLM/GLM_FL.R")
 #sign=cut(pvalue,c(0,0.001,0.01,0.05,0.1,1),include.lowest = T,labels=c('***','**','*','.',''))
 #cbind("Estimate"=beta$coef[,ncol(beta$coef)],"Std. Error"=beta$se,"z value"=zvalue,"Pr(>|z|)"=pvalue,""=sign)
 
@@ -22,33 +22,20 @@ M_1=NULL
 f=num_awards ~ prog + math
 #iterations
 for(j in 1:maxit){
-  N_1=node_beta(formula = f,Data = p1,beta = M_1$coef,iter = j,reg_type = 'poisson')
-  N_2=node_beta(formula = f,Data = p2,beta = M_1$coef,iter = j,reg_type = 'poisson')
-  M_1=master_beta(N_1,N_2,beta = M_1,reg_type = 'poisson')
-  D_1=node_deviance(formula = f,Data = p1,beta = M_1$coef,iter = j,reg_type = 'poisson')
-  D_2=node_deviance(formula = f,Data = p2,beta = M_1$coef,iter = j,reg_type = 'poisson')
-  M_2=master_convergence(D_1,D_2,beta=M_1,iter=j,reg_type = 'poisson',formula = f,maxit=maxit)
-  if(M_2$convergence)break
+  N_1=node_beta(formula = f,Data = p1,beta = M_1$coef,iter = j,family = 'poisson')
+  N_2=node_beta(formula = f,Data = p2,beta = M_1$coef,iter = j,family = 'poisson')
+  M_1=master_beta(N_1,N_2,beta = M_1,family = 'poisson')
+  D_1=node_deviance(formula = f,Data = p1,beta = M_1$coef,iter = j,family = 'poisson')
+  D_2=node_deviance(formula = f,Data = p2,beta = M_1$coef,iter = j,family = 'poisson')
+  M_2=master_convergence(D_1,D_2,beta=M_1,iter=j,family = 'poisson',formula = f,maxit=maxit)
+  if(M_2$converged)break
 }
+
 Summary_FL_GLM(M_2)
-
 summary(m <- glm(num_awards ~ prog + math, family="poisson", data=p))
-#summary(m <- glm(num_awards ~ 1, family="poisson", data=p))
-
-(s1 <- data.frame(math = mean(p$math),
-                  prog = factor(1:3, levels = 1:3, labels = levels(p$prog))))
 
 predict(m, type="response")[1:10]
-
-exp(m$coefficients[1]+
-  m$coefficients[2]*(p$prog=='Academic')+
-  m$coefficients[3]*(p$prog=='Vocational')+
-  m$coefficients[4]*p$math)[1:10]
-
-exp(M_2$coefficients[1]+
-      M_2$coefficients[2]*(p$prog=='Academic')+
-      M_2$coefficients[3]*(p$prog=='Vocational')+
-      M_2$coefficients[4]*p$math)[1:10]
+predict(as.GLM(M_2),type = 'response',p[1:10,])
 
 #####binomial example
 
@@ -63,13 +50,13 @@ M_1=NULL
 f=admit ~ gre+gpa+rank
 #iterations
 for(j in 1:maxit){
-  N_1=node_beta(formula =f,Data = df1,beta = M_1$coef,iter = j,reg_type = 'binomial')
-  N_2=node_beta(formula = f,Data = df2,beta = M_1$coef,iter = j,reg_type = 'binomial')
-  M_1=master_beta(N_1,N_2,beta = M_1,reg_type = 'binomial')
-  D_1=node_deviance(formula = f,Data = df1,beta = M_1$coef,iter = j,reg_type = 'binomial')
-  D_2=node_deviance(formula = f,Data = df2,beta = M_1$coef,iter = j,reg_type = 'binomial')
-  M_2=master_convergence(D_1,D_2,beta=M_1,iter=j,reg_type = 'binomial',formula = f,maxit=maxit)
-  if(M_2$convergence)break
+  N_1=node_beta(formula =f,Data = df1,beta = M_1$coef,iter = j,family = 'binomial')
+  N_2=node_beta(formula = f,Data = df2,beta = M_1$coef,iter = j,family = 'binomial')
+  M_1=master_beta(N_1,N_2,beta = M_1,family = 'binomial')
+  D_1=node_deviance(formula = f,Data = df1,beta = M_1$coef,iter = j,family = 'binomial')
+  D_2=node_deviance(formula = f,Data = df2,beta = M_1$coef,iter = j,family = 'binomial')
+  M_2=master_convergence(D_1,D_2,beta=M_1,iter=j,family = 'binomial',formula = f,maxit=maxit)
+  if(M_2$converged)break
 }
 Summary_FL_GLM(M_2)
 summary(logit <- glm(admit ~ gre+gpa+rank,data=df,family="binomial"))
@@ -77,7 +64,7 @@ summary(logit <- glm(admit ~ gre+gpa+rank,data=df,family="binomial"))
 
 
 #####gaussian example
-
+library(MASS)
 df <- anorexia
 
 df1=anorexia[1:(nrow(anorexia)/2),]
@@ -88,13 +75,13 @@ M_1=NULL
 f=Postwt ~ Prewt + Treat + offset(Prewt)
 #iterations
 for(j in 1:maxit){
-  N_1=node_beta(formula =f,Data = df1,beta = M_1$coef,iter = j,reg_type = 'gaussian')
-  N_2=node_beta(formula = f,Data = df2,beta = M_1$coef,iter = j,reg_type = 'gaussian')
-  M_1=master_beta(N_1,N_2,beta = M_1,reg_type = 'gaussian')
-  D_1=node_deviance(formula = f,Data = df1,beta = M_1$coef,iter = j,reg_type = 'gaussian')
-  D_2=node_deviance(formula = f,Data = df2,beta = M_1$coef,iter = j,reg_type = 'gaussian')
-  M_2=master_convergence(D_1,D_2,beta=M_1,iter=j,reg_type = 'gaussian',formula = f,maxit=maxit,tol=tol)
-  if(M_2$convergence)break
+  N_1=node_beta(formula =f,Data = df1,beta = M_1$coef,iter = j,family = 'gaussian')
+  N_2=node_beta(formula = f,Data = df2,beta = M_1$coef,iter = j,family = 'gaussian')
+  M_1=master_beta(N_1,N_2,beta = M_1,family = 'gaussian')
+  D_1=node_deviance(formula = f,Data = df1,beta = M_1$coef,iter = j,family = 'gaussian')
+  D_2=node_deviance(formula = f,Data = df2,beta = M_1$coef,iter = j,family = 'gaussian')
+  M_2=master_convergence(D_1,D_2,beta=M_1,iter=j,family = 'gaussian',formula = f,maxit=maxit,tol=tol)
+  if(M_2$converged)break
 }
 Summary_FL_GLM(M_2)
 summary(anorex.1 <- glm(Postwt ~ Prewt + Treat + offset(Prewt),family = gaussian, data = anorexia))
--- GLM_FL.R
+++ GLM_FL.R
@@ -1,11 +1,17 @@
-node_beta=function(formula,Data,weights=NULL,reg_type=c('gaussian','poisson','binomial'),beta=NULL,iter){
-  y=eval(formula[[2]], envir = Data)
-  X=model.matrix(formula,data = Data)
-  offset=model.offset(model.frame(formula,data = Data))
-  #reg_type <- match.arg(reg_type)
-  if(reg_type=='gaussian') family = gaussian()
-  if(reg_type=='poisson')  family = poisson(log)
-  if(reg_type=='binomial') family = binomial()
+node_beta=function(formula,Data,weights=NULL,family=gaussian,beta=NULL,iter){
+  y=eval(formula[[2]], envir = Data) #extract y and X varibales name from formula
+  X=model.matrix(formula,data = Data) #create a model matrix 
+  offset=model.offset(model.frame(formula,data = Data)) #extract the offset from formula (if exists)
+  
+  if (is.character(family)) 
+    family <- get(family, mode = "function", envir = parent.frame())
+  if (is.function(family)) 
+    family <- family()
+  if (is.null(family$family)) {
+    print(family)
+    stop("'family' not recognized")
+  }
+  
   if (is.null(weights)) weights <- rep.int(1, nrow(X))
   if (is.null(offset)) offset <- rep.int(0, nrow(X))
   nobs = nrow(X)    # needed by the initialize expression below
@@ -27,7 +33,15 @@ node_beta=function(formula,Data,weights=NULL,reg_type=c('gaussian','poisson','bi
   return(list(v1=crossprod(X,W*X),v2=crossprod(X,W*z),dispersion=dispersion,nobs=nobs,nvars=nvars))
 }
 
-master_beta=function(...,beta=NULL,reg_type=c('gaussian','poisson','binomial')){
+master_beta=function(...,beta=NULL,family=gaussian){
+  if (is.character(family)) 
+    family <- get(family, mode = "function", envir = parent.frame())
+  if (is.function(family)) 
+    family <- family()
+  if (is.null(family$family)) {
+    print(family)
+    stop("'family' not recognized")
+  }
   g <- list(...)
   a=Reduce(`+`,lapply(1:length(g),function(j) g[[j]]$v1))
   b=Reduce(`+`,lapply(1:length(g),function(j) g[[j]]$v2))
@@ -39,7 +53,7 @@ master_beta=function(...,beta=NULL,reg_type=c('gaussian','poisson','binomial')){
   }else{
     beta=beta$coef
   }
-  if(reg_type %in% c('poisson','binomial')){
+  if(family$family %in% c('poisson','binomial')){
       disp=1
       est.disp=FALSE
     }else{
@@ -51,13 +65,18 @@ master_beta=function(...,beta=NULL,reg_type=c('gaussian','poisson','binomial')){
   return(list(coef=cbind(beta,f),se=se,disp=disp,est.disp=est.disp,nobs=nobs,nvars=nvars))
 }
 
-node_deviance=function(formula,Data,weights=NULL,reg_type=c('gaussian','poisson','binomial'),beta,iter){
+node_deviance=function(formula,Data,weights=NULL,family=gaussian,beta,iter){
   y=eval(formula[[2]], envir = Data)
   X=model.matrix(formula,data = Data)
   offset=model.offset(model.frame(formula,data = Data)) 
-  if(reg_type=='gaussian') family = gaussian()
-  if(reg_type=='poisson')  family = poisson(log)
-  if(reg_type=='binomial') family = binomial()
+  if (is.character(family)) 
+    family <- get(family, mode = "function", envir = parent.frame())
+  if (is.function(family)) 
+    family <- family()
+  if (is.null(family$family)) {
+    print(family)
+    stop("'family' not recognized")
+  }
   if (is.null(weights)) weights <- rep.int(1, nrow(X))
   if (is.null(offset)) offset <- rep.int(0, nrow(X))
   
@@ -79,13 +98,21 @@ node_deviance=function(formula,Data,weights=NULL,reg_type=c('gaussian','poisson'
   return(list(dev_old=dev_old,dev=dev))
 }
 
-master_convergence=function(...,tol=1e-16,beta,iter,reg_type=c('gaussian','poisson','binomial'),formula,maxit){
+master_convergence=function(...,tol= 1e-08,beta,iter,family=gaussian,formula,maxit=25){
+  if (is.character(family)) 
+    family <- get(family, mode = "function", envir = parent.frame())
+  if (is.function(family)) 
+    family <- family()
+  if (is.null(family$family)) {
+    print(family)
+    stop("'family' not recognized")
+  }
   x <- list(...)
   dev_old=Reduce(`+`,lapply(1:length(x),function(j) x[[j]]$dev_old))
   dev=Reduce(`+`,lapply(1:length(x),function(j) x[[j]]$dev))
   convergence=(abs(dev - dev_old) / (0.1 + abs(dev)) < tol)
   if(convergence==FALSE & iter<maxit){
-    return(list(convergence=convergence))
+    return(list(converged=convergence))
   }else{
    zvalue <- beta$coef[,ncol(beta$coef)]/beta$se
     if(beta$est.disp){
@@ -93,30 +120,170 @@ master_convergence=function(...,tol=1e-16,beta,iter,reg_type=c('gaussian','poiss
     }else{
       pvalue <- 2 * pnorm(-abs(zvalue))
     }
-    L=list(convergence=convergence,
-    coefficients=beta$coef[,ncol(beta$coef)],
-    Std.Error=beta$se,
-    pvalue=pvalue,
-    zvalue=zvalue,
-    dispersion=beta$disp,
-    est.disp=beta$est.disp,
-    model=formula,
-    regression=reg_type,
-    iter=iter,
-    dev_res=dev,
-    nobs=beta$nobs,
-    nvars=beta$nvars)
+   L=list(converged=convergence,
+          coefficients=beta$coef[,ncol(beta$coef)],
+          Std.Error=beta$se,
+          pvalue=pvalue,
+          zvalue=zvalue,
+          dispersion=beta$disp,
+          est.disp=beta$est.disp,
+          formula=formula,
+          family=family,
+          iter=iter,
+          deviance=dev,
+          null.deviance=dev,
+          nobs=beta$nobs,
+          nvars=beta$nvars)
   return(L)
   }
  }
 
-Summary_FL_GLM <- function(x, ...){
-  sign=cut(x$pvalue,c(0,0.001,0.01,0.05,0.1,1),include.lowest = T,labels=c('***','**','*','.',''))
-    cat("Model:\n")
-  print(x$model)
+as.GLM <- function(obj, data=NULL) {
   
-  DF=data.frame(round(x$coefficients,5),round(x$Std.Error,5),round(x$zvalue,3),round(x$pvalue,5),sign)
-  if(x$est.disp){
+  dots <- as.list(obj$coefficients)
+  
+  out<-list()
+  
+  tt <- terms(obj$formula, data=data)
+  
+  if(!is.null(data)) {
+    
+    mf <- model.frame(tt, data)
+    
+    vn <- sapply(attr(tt, "variables")[-1], deparse)
+    
+    
+    tt=M_2$terms
+    vn <- sapply(attr(tt, "variables")[-1], deparse)
+    
+    
+    
+    if((yvar <- attr(tt, "response"))>0)
+      
+      vn <- vn[-yvar]
+    
+    xlvl <- lapply(data[vn], function(x) if (is.factor(x))
+      
+      levels(x)
+      
+      else if (is.character(x))
+        
+        levels(as.factor(x))
+      
+      else
+        
+        NULL)
+    
+    attr(out, "xlevels") <- xlvl[!vapply(xlvl,is.null,NA)]
+    
+    attr(tt, "dataClasses") <- sapply(data[vn], stats:::.MFclass)
+    
+  }
+  
+  out$terms <- tt
+  
+  coef <- numeric(0)
+  
+  stopifnot(length(dots)>1 & !is.null(names(dots)))
+  
+  for(i in seq_along(dots)) {
+    
+    if((n<-names(dots)[i]) != "") {
+      
+      v <- dots[[i]]
+      
+      if(!is.null(names(v))) {
+        
+        coef[paste0(n, names(v))] <- v
+        
+      } else {
+        
+        stopifnot(length(v)==1)
+        
+        coef[n] <- v
+        
+      }
+      
+    } else {
+      
+      coef["(Intercept)"] <- dots[[i]]
+      
+    }   
+    
+  }
+  
+  out$coefficients <- coef
+  
+  out$rank <- length(coef)
+  
+  family=obj$family
+  
+  if (!missing(family)) {
+    
+    out$family <- if (class(family) == "family") {
+      
+      family
+      
+    } else if (class(family) == "function") {
+      
+      family()
+      
+    } else if (class(family) == "character") {
+      
+      get(family)()
+      
+    } else {
+      
+      stop(paste("invalid family class:", class(family)))
+      
+    }
+    
+    out$qr <- list(pivot=seq_len(out$rank))
+    
+    out$deviance <- obj$deviance
+    
+    out$null.deviance <- obj$null.deviance
+    
+    out$aic <- 1
+    
+    out$iter=obj$iter
+    
+    out$df.null=obj$nobs-1
+    
+    out$df.residual=obj$nobs-out$rank
+    
+    out$call=call("glm_FL",f,family=obj$family$family)
+    
+    class(out) <- c("glm","lm")
+    
+    
+  } else {
+    
+    class(out) <- "lm"
+    
+    out$fitted.values <- predict(out, newdata=dd)
+    
+    out$residuals <- out$mf[attr(tt, "response")] - out$fitted.values
+    
+    out$df.residual <- nrow(data) - out$rank
+    
+    out$model <- data
+    
+    #QR doesn't work
+    
+  }
+  
+  out
+  
+}
+
+Summary_FL_GLM <- function(obj, ...){
+  sign=cut(obj$pvalue,c(0,0.001,0.01,0.05,0.1,1),include.lowest = T,labels=c('***','**','*','.',''))
+    cat("Model:\n")
+  print(obj$formula)
+  
+  DF=data.frame(round(obj$coefficients,5),round(obj$Std.Error,5),round(obj$zvalue,3),round(obj$pvalue,5),sign)
+  if(obj$est.disp){
     names(DF)=c("Estimate","Std. Error","t value","Pr(>|t|)","")
   }else{
     names(DF)=c("Estimate","Std. Error","z value","Pr(>|z|)","")
@@ -126,10 +293,32 @@ Summary_FL_GLM <- function(x, ...){
   
   cat("---\n")
   cat("Signif. codes: 0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1 \n\n")
-  cat(paste0('(Dispersion parameter for ',x$regression," family taken to be ",round(x$dispersion,5),')\n \n'))
-  cat(paste0("Residual deviance: ",round(x$dev_res,2),"  on ",x$nobs-x$nvars," degrees of freedom \n \n"))
-  cat(paste0('Number of Fisher Scoring iterations: ',x$iter,'\n'))
+  cat(paste0('(Dispersion parameter for ',obj$regression," family taken to be ",round(obj$dispersion,5),')\n \n'))
+  cat(paste0("Residual deviance: ",round(obj$deviance,2),"  on ",obj$nobs-obj$nvars," degrees of freedom \n \n"))
+  cat(paste0('Number of Fisher Scoring iterations: ',obj$iter,'\n'))
 
 }
 
 
+# 
+# glm_obj=list(
+#   call=paste0('glm_FL(formula = ',Reduce(paste, deparse(formula)),', family = ',reg_type,')'),
+#   coefficients=beta$coef[,ncol(beta$coef)],
+#   rank=beta$nvars+1,
+#   family=reg_type,
+#   deviance=dev,
+#   null.deviance=dev,
+#   iter=iter,
+#   df.null=beta$nobs-1,
+#   df.residual=beta$nobs-beta$nvars,
+#   converged=convergence,
+#   formula=formula,
+#   aic=0,
+#   control=list(epsilon=tol,maxit=maxit),
+#   terms=terms.formula(f)
+# ) 
+# L=structure(glm_obj, class = "glm")
+
+
+# 
+# 
